b14873923ae12bd453e29d3d9d3f448b8399bb5b
aleksey <av.pavlow@gmail.com>
2025-11-21 13:11:50 +0300

    refact: Добавить распараллеливание вычисления - Добавлено распараллеливание вычислений

--- internal/autoencoder/autoencoder.go
+++ internal/autoencoder/autoencoder.go
@@ -10,7 +10,6 @@ import (
 )
 
 // Autoencoder представляет простой полносвязный автоэнкодер с одним скрытым слоем.
-// Состоит из энкодера (W1, b1) и декодера (W2, b2).
 type Autoencoder struct {
 	inputSize  int // размер входного вектора
 	latentSize int // размер скрытого (латентного) слоя
@@ -22,12 +21,14 @@ type Autoencoder struct {
 	b2 []float64   // bias выходного слоя
 }
 
+// Encode кодирует входной вектор x в скрытое представление.
 func (ae *Autoencoder) Encode(x [][]float64) [][]float64 {
 	z1 := mathutils.MatMul(x, ae.W1)  // линейная трансформация
 	a1 := mathutils.SigmoidMatrix(z1) // активация
 	return a1
 }
 
+// Decode восстанавливает данные из скрытого представления.
 func (ae *Autoencoder) Decode(latent [][]float64) [][]float64 {
 	z2 := mathutils.MatMul(latent, ae.W2)
 	out := mathutils.SigmoidMatrix(z2)
@@ -35,7 +36,6 @@ func (ae *Autoencoder) Decode(latent [][]float64) [][]float64 {
 }
 
 // NewAutoencoder создаёт новый автоэнкодер с заданными размерами входного и скрытого слоя.
-// Веса инициализируются случайно с помощью Xavier/He, bias инициализируются нулями.
 func NewAutoencoder(inputSize, latentSize int) *Autoencoder {
 	return &Autoencoder{
 		inputSize:  inputSize,
@@ -51,53 +51,34 @@ func NewAutoencoder(inputSize, latentSize int) *Autoencoder {
 
 // Forward выполняет прямой проход автоэнкодера для батча входных данных x.
 func (ae *Autoencoder) Forward(x [][]float64) ([][]float64, [][]float64, [][]float64, [][]float64) {
-
 	// --- Энкодер ---
-	// z1 = xW1 + b1
 	z1 := mathutils.AddBias(mathutils.MatMul(x, ae.W1), ae.b1)
-
-	// a1 = sigmoid(z1)
-	a1 := make([][]float64, len(z1))
-	for i := range z1 {
-		a1[i] = make([]float64, len(z1[i]))
-		for j := range z1[i] {
-			a1[i][j] = mathutils.Sigmoid(z1[i][j])
-		}
-	}
+	a1 := mathutils.SigmoidMatrix(z1)
 
 	// --- Декодер ---
-	// z2 = a1W2 + b2
 	z2 := mathutils.AddBias(mathutils.MatMul(a1, ae.W2), ae.b2)
-
-	// out = sigmoid(z2)
-	out := make([][]float64, len(z2))
-	for i := range z2 {
-		out[i] = make([]float64, len(z2[i]))
-		for j := range z2[i] {
-			out[i][j] = mathutils.Sigmoid(z2[i][j])
-		}
-	}
+	out := mathutils.SigmoidMatrix(z2)
 
 	return a1, out, z1, z2
 }
 
-// TrainStep выполняет один шаг обучения автоэнкодера на батче x с заданной скоростью обучения lr.
-// Выполняет forward pass, вычисление градиентов методом обратного распространения и обновление весов.
-// Возвращает среднеквадратичную ошибку (MSE) по батчу.
+// TrainStep выполняет один шаг обучения с использованием батча данных x.
 func (ae *Autoencoder) TrainStep(x [][]float64, lr float64) float64 {
 	a1, out, z1, z2 := ae.Forward(x)
 
+	// Параллельное вычисление градиентов
 	dOut, mse := ae.computeOutputGradient(out, x, z2)
 	dW2, db2 := ae.computeGradientsW2(a1, dOut)
 	dA1 := ae.backpropHidden(dOut, z1)
 	dW1, db1 := ae.computeGradientsW1(x, dA1)
 
+	// Параллельное обновление весов
 	ae.updateWeights(dW1, db1, dW2, db2, lr)
 
 	return mse
 }
 
-// computeOutputGradient вычисляет градиент ошибки на выходе и среднеквадратичную ошибку (MSE).
+// computeOutputGradient вычисляет градиент ошибки на выходе и MSE.
 func (ae *Autoencoder) computeOutputGradient(out, x, z2 [][]float64) ([][]float64, float64) {
 	mse := 0.0
 	dOut := make([][]float64, len(out))
@@ -113,7 +94,7 @@ func (ae *Autoencoder) computeOutputGradient(out, x, z2 [][]float64) ([][]float6
 	return dOut, mse
 }
 
-// computeGradientsW2 вычисляет градиенты весов и bias декодера.
+// computeGradientsW2 вычисляет градиенты для весов W2 и bias b2.
 func (ae *Autoencoder) computeGradientsW2(a1, dOut [][]float64) ([][]float64, []float64) {
 	dW2 := make([][]float64, ae.latentSize)
 	for i := range dW2 {
@@ -121,14 +102,26 @@ func (ae *Autoencoder) computeGradientsW2(a1, dOut [][]float64) ([][]float64, []
 	}
 	db2 := make([]float64, ae.inputSize)
 
-	for i := 0; i < len(a1); i++ {
-		for j := 0; j < ae.inputSize; j++ {
-			db2[j] += dOut[i][j]
-			for k := 0; k < ae.latentSize; k++ {
-				dW2[k][j] += a1[i][k] * dOut[i][j]
+	var wg sync.WaitGroup
+	numWorkers := runtime.NumCPU()
+
+	for w := 0; w < numWorkers; w++ {
+		wg.Add(1)
+		go func(w int) {
+			defer wg.Done()
+			start := w * len(a1) / numWorkers
+			end := (w + 1) * len(a1) / numWorkers
+			for i := start; i < end; i++ {
+				for j := 0; j < ae.inputSize; j++ {
+					db2[j] += dOut[i][j]
+					for k := 0; k < ae.latentSize; k++ {
+						dW2[k][j] += a1[i][k] * dOut[i][j]
+					}
+				}
 			}
-		}
+		}(w)
 	}
+	wg.Wait()
 	return dW2, db2
 }
 
@@ -140,7 +133,7 @@ func (ae *Autoencoder) backpropHidden(dOut, z1 [][]float64) [][]float64 {
 	}
 
 	var wg sync.WaitGroup
-	numWorkers := runtime.NumCPU() // или len(dOut) если батч меньше
+	numWorkers := runtime.NumCPU()
 
 	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
@@ -155,12 +148,11 @@ func (ae *Autoencoder) backpropHidden(dOut, z1 [][]float64) [][]float64 {
 			}
 		}(i)
 	}
-
-	wg.Wait() // ждём, пока все горутины завершатся
+	wg.Wait()
 	return dA1
 }
 
-// Параллельные вычисления выполняются по батчу и по нейронам.
+// computeGradientsW1 вычисляет градиенты для весов W1 и bias b1.
 func (ae *Autoencoder) computeGradientsW1(x, dA1 [][]float64) ([][]float64, []float64) {
 	dW1 := make([][]float64, ae.inputSize)
 	for i := range dW1 {
@@ -168,57 +160,33 @@ func (ae *Autoencoder) computeGradientsW1(x, dA1 [][]float64) ([][]float64, []fl
 	}
 	db1 := make([]float64, ae.latentSize)
 
+	var wg sync.WaitGroup
 	numWorkers := runtime.NumCPU()
-	wg := sync.WaitGroup{}
-	chunkSize := (len(x) + numWorkers - 1) / numWorkers
 
 	for w := 0; w < numWorkers; w++ {
-		start := w * chunkSize
-		end := start + chunkSize
-		if end > len(x) {
-			end = len(x)
-		}
-
 		wg.Add(1)
-		go func(start, end int) {
+		go func(w int) {
 			defer wg.Done()
-			localDW := make([][]float64, ae.inputSize)
-			for i := range localDW {
-				localDW[i] = make([]float64, ae.latentSize)
-			}
-			localDB := make([]float64, ae.latentSize)
-
+			start := w * len(x) / numWorkers
+			end := (w + 1) * len(x) / numWorkers
 			for i := start; i < end; i++ {
 				for j := 0; j < ae.latentSize; j++ {
-					localDB[j] += dA1[i][j]
+					db1[j] += dA1[i][j]
 					for k := 0; k < ae.inputSize; k++ {
-						localDW[k][j] += x[i][k] * dA1[i][j]
+						dW1[k][j] += x[i][k] * dA1[i][j]
 					}
 				}
 			}
-
-			// Суммируем локальные градиенты в глобальные
-			for i := 0; i < ae.inputSize; i++ {
-				for j := 0; j < ae.latentSize; j++ {
-					dW1[i][j] += localDW[i][j]
-				}
-			}
-			for j := 0; j < ae.latentSize; j++ {
-				db1[j] += localDB[j]
-			}
-		}(start, end)
+		}(w)
 	}
-
 	wg.Wait()
 	return dW1, db1
 }
 
-// updateWeights обновляет веса и bias автоэнкодера с использованием градиентов и скорости обучения lr.
-
+// updateWeights обновляет веса и bias автоэнкодера с использованием градиентов.
 func (ae *Autoencoder) updateWeights(dW1 [][]float64, db1 []float64, dW2 [][]float64, db2 []float64, lr float64) {
 	var wg sync.WaitGroup
 
-	// Обновляем W1 параллельно
 	for i := 0; i < ae.inputSize; i++ {
 		wg.Add(1)
 		go func(i int) {
@@ -229,7 +197,6 @@ func (ae *Autoencoder) updateWeights(dW1 [][]float64, db1 []float64, dW2 [][]flo
 		}(i)
 	}
 
-	// Обновляем b1 параллельно
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
@@ -238,7 +205,6 @@ func (ae *Autoencoder) updateWeights(dW1 [][]float64, db1 []float64, dW2 [][]flo
 		}
 	}()
 
-	// Обновляем W2 параллельно
 	for i := 0; i < ae.latentSize; i++ {
 		wg.Add(1)
 		go func(i int) {
@@ -249,7 +215,6 @@ func (ae *Autoencoder) updateWeights(dW1 [][]float64, db1 []float64, dW2 [][]flo
 		}(i)
 	}
 
-	// Обновляем b2 параллельно
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
@@ -258,11 +223,10 @@ func (ae *Autoencoder) updateWeights(dW1 [][]float64, db1 []float64, dW2 [][]flo
 		}
 	}()
 
-	// Ждём завершения всех горутин
 	wg.Wait()
 }
 
-// Save сохраняет веса и bias автоэнкодера в файл
+// Save сохраняет веса и bias автоэнкодера в файл.
 func (ae *Autoencoder) Save(filename string) error {
 	file, err := os.Create(filename)
 	if err != nil {
@@ -274,7 +238,7 @@ func (ae *Autoencoder) Save(filename string) error {
 	return encoder.Encode(ae)
 }
 
-// Load загружает веса и bias из файла
+// Load загружает веса и bias из файла.
 func (ae *Autoencoder) Load(filename string) error {
 	file, err := os.Open(filename)
 	if err != nil {
